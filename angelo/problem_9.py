006d0ff568034404d2d3958ff230c7102458dca8
Angelo Compagnucci <angelo.compagnucci@gmail.com>
2021-10-04 22:56:51 +0200

    Adding project euler problem 9 solution
    Signed-off-by: Angelo Compagnucci <angelo.compagnucci@gmail.com>

---
+++ angelo/problem_9.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for a in range(1, 400):
+    for b in range(1, 400):
+        c = (1000 - a) - b
+        if a < b < c:
+            if a**2 + b**2 == c**2:
+                print(a * b * c)
